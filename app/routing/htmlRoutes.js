407509e17cb79e63d0b2791e0bbf8bfdbbb4f026
mollie chacon <mollie.mosley@gmail.com>
2017-10-26 20:21:14 -0600

    full functionality

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
 var path = require("path");
 
 module.exports = function(app) {
-    api.get("/survey", function(req, res) {
+    app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     
-    api.get("*", function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
--- server.js
+++ server.js
@@ -11,10 +11,18 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // require our html routes
-require("./routes/htmlRoutes")(app);
-require("./routes/apiRoutes")(app);
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
 
